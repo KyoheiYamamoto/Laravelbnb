c8ed283ef02387101db4f8bea11528e907adedb8
KyoheiYamamoto <arienkyohei@gmail.com>
2020-12-01 14:09:42 +0900

    Component props and prop types

---
+++ resources/js/bookables/Bookables.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <bookable-list-item
+      item-title="Cheap villa"
+      item-content=" A very good"
+      v-bind:price="1000"
+    ></bookable-list-item>
+    <bookable-list-item
+      item-title="Cheap villa2"
+      item-content=" A very good2"
+      v-bind:price="1500"
+    ></bookable-list-item>
+  </div>
+</template>
+
+<script>
+import BookableListItem from "./BookableListItem";
+
+export default {
+  components: {
+    BookableListItem,
+  },
+};
+</script>
