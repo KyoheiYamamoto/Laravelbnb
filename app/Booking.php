456682655d25e3ed73b6895b4c0bd9075fe52095
KyoheiYamamoto <arienkyohei@gmail.com>
2020-12-18 10:18:59 +0900

    フォーム、イベント、Eloquentリレーション、検証、クエリスコープ、エラーの処理実装完了

--- app/Booking.php
+++ app/Booking.php
@@ -2,14 +2,21 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    protected $fillable = ['from','to'];
+    protected $fillable = ['from', 'to'];
 
     public function bookable()
     {
-        return $this->belongsTo(Bookabke::class);
+        return $this->belongsTo(Bookable::class);
+    }
+
+    public function scopeBetweenDates(Builder $query, $from, $to)
+    {
+        return $query->where('to', '>=', $from)
+            ->where('from', '<=', $to);
     }
 }
