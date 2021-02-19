e5e6a1c5afdc3e327a95e7733168961f7cd51e02
KyoheiYamamoto <arienkyohei@gmail.com>
2021-02-19 15:56:45 +0900

    FatalError component (custom Vue component for unhandled errors)

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import Index from "./Index.vue";
 import router from "./routes";
 import  moment from "moment";
 
+import FatalError from "./shared/components/FatalError";
 import StarRating from "./shared/components/StarRating";
 
 
@@ -14,6 +15,7 @@ Vue.use(VueRouter);
 
 Vue.filter("fromNow", value => moment(value).fromNow());
 
+Vue.component("fatal-error", FatalError);
 Vue.component("star-rating", StarRating);
 
 
