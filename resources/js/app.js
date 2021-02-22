9343cd678e54f31597691f03b6215d10b2d3c726
KyoheiYamamoto <arienkyohei@gmail.com>
2021-02-22 23:52:16 +0900

    vue-component-slots

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import Index from "./Index.vue";
 import router from "./routes";
 import  moment from "moment";
 import FatalError from "./shared/components/FatalError";
+import Success from "./shared/components/Success";
 import StarRating from "./shared/components/StarRating";
 import ValidationErrors from "./shared/components/ValidationErrors";
 
@@ -16,6 +17,7 @@ Vue.use(VueRouter);
 Vue.filter("fromNow", value => moment(value).fromNow());
 
 Vue.component("fatal-error", FatalError);
+Vue.component("success", Success);
 Vue.component("star-rating", StarRating);
 Vue.component("v-errors", ValidationErrors);
 
