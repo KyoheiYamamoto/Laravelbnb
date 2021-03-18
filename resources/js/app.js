25dbd4de7b529d04ee56214810e3ee22d9381388
KyoheiYamamoto <arienkyohei@gmail.com>
2021-03-18 13:35:12 +0900

    vuex導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ require('./bootstrap');
 
 import VueRouter from "vue-router";
 import Index from "./Index.vue";
+import Vuex from "vuex"
 import router from "./routes";
 import  moment from "moment";
 import FatalError from "./shared/components/FatalError";
@@ -13,6 +14,7 @@ import ValidationErrors from "./shared/components/ValidationErrors";
 window.Vue = require("vue");
 
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 Vue.filter("fromNow", value => moment(value).fromNow());
 
